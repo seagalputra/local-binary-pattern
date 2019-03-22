a158871091d4d7da41b998f70659cc99e54b5e2d
seagalputra <seagalputra@gmail.com>
2019-03-22 18:14:25 +0700

    Fix classifier

--- lib/kNNClassifier.m
+++ lib/kNNClassifier.m
@@ -1,27 +1,7 @@
-%% Preprocessing data
-clear;
-clc;
-close all;
+function predict = kNNClassifier(dataTrain, dataValidation, labelTrain, k)
+% kNearest Neighbors Algorithm
 
-data = xlsread('..\data\dataset.xlsx', 'DataTrain');
-
-% shuffle data
-idx = randperm(size(data,1));
-data = data(idx,:);
-
-% separate data and label
-feature = data(:,1:4);
-label = data(:,5);
-
-%% split data into training and validation
-dataTrain = feature(1:3000,:);
-dataValidation = feature(3001:4000,:);
-labelTrain = label(1:3000,:);
-labelValidation = label(3001:4000,:);
-
-%% kNearest Neighbors Algorithm
 % calculate distance
-k = 3;
 euclidean = @(x,y) sqrt(sum((x-y).^2));
 for i = 1:size(dataValidation,1)
     for j = 1:size(dataTrain,1)
@@ -40,4 +20,4 @@ for i = 1:size(dataValidation,1)
     end
 end
 
-%% Compute accuracy
+end
