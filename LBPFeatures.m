53ef1b20523d250786e342c0a6ec8f824defa2ee
seagalputra <seagalputra@gmail.com>
2019-07-12 09:44:46 +0700

    Add parameter in extract feature lbp

--- LBPFeatures.m
+++ LBPFeatures.m
@@ -6,30 +6,19 @@ close all;
 addpath('lib');
 load(fullfile('data', 'mangga.mat'));
 
-%% Using texture descriptor extract feature vector using Local Binary Pattern
-
-% TODO : Fix this thing
-% for i = 1:size(imageData,2)
-%     tempLbp = localBinaryPattern(imageData{i});
-%     imageLbp{i} = uint8(tempLbp);
-%     
-%     % Split image into separate patch
-%     imageSplit = createImagePatch(imageLbp{i},4,4);
-% 
-%     % Create histogram from patch
-%     for j = 1:size(imageSplit,2)
-%         features(i,:) = createFeatureFromHistogram(imageSplit{j});
-%     end
-%     disp(['image - ', num2str(i), ' extracted..']);
-% end
-
 %% Ambil ciri LBP dengan lib Matlab
+
+% PARAMETER
+NumNeighbors = 8; % range antara 4 sampai 24
+Radius = 1; % range antara 1 sampai 5
+
 for i = 1:size(imageData,2)
     grayImage = rgb2gray(imageData{i});
-    lbpFeatures{i,:} = extractLBPFeatures(grayImage);
+    lbp_features{i,:} = extractLBPFeatures(grayImage, 'Radius', Radius, ...
+        'NumNeighbors', NumNeighbors);
     disp(['LBP features from image - ', num2str(i) ,' successfully extracted']);
 end
-features = cell2mat(lbpFeatures);
+features = cell2mat(lbp_features);
 
 %% Process data
 % Shuffle data
@@ -38,21 +27,35 @@ filename = listName(idx);
 features = features(idx,:);
 label = label(idx,:);
 % Split data
-splitRatio = 0.8;
-numFeatures = size(features,1);
-numDataTrain = numFeatures * splitRatio;
-numDataTest = numFeatures - numDataTrain;
-% Split fitur dan label
-dataTrain = features(1:numDataTrain,:);
-dataTest = features(numDataTrain+1:numFeatures,:);
-labelTrain = label(1:numDataTrain,:);
-labelTest = label(numDataTrain+1:numFeatures,:);
-% Split filename train dan test
-filenameTrain = filename(1:numDataTrain);
-filenameTest = filename(numDataTrain+1:numFeatures);
+split_ratio = 0.8;
+num_features = size(features,1);
+% untuk data train
+num_data_train = num_features * split_ratio;
+data_train = features(1:num_data_train,:);
+label_train = label(1:num_data_train,:);
+filename_train = filename(1:num_data_train);
 
-% TODO: seimbangkan banyaknya data test antara indramayu dan harumanis
+% untuk data test
+num = 1 - split_ratio;
+test_num = int64(num * size(features,1));
+indramayu = find(label == 0);
+harumanis = find(label == 1);
+rand_indramayu = randperm(size(indramayu,1), test_num/2);
+rand_harumanis = randperm(size(harumanis,1), test_num/2);
 
-save('fiturLBP.mat', 'dataTrain', ...
-    'dataTest', 'labelTrain', 'labelTest', ...
-    'filenameTrain', 'filenameTest');
+idx_indramayu = indramayu(rand_indramayu);
+idx_harumanis = harumanis(rand_harumanis);
+test_indramayu = features(idx_indramayu,:);
+test_harumanis = features(idx_harumanis,:);
+label_indramayu = label(idx_indramayu,:);
+label_harumanis = label(idx_harumanis,:);
+filename_indramayu = filename(:,idx_indramayu);
+filename_harumanis = filename(:,idx_harumanis);
+data_test = [test_indramayu; test_harumanis];
+label_test = [label_indramayu; label_harumanis];
+filename_test = [filename_indramayu, filename_harumanis];
+
+% save mat dataset
+save('fiturLBP.mat', 'data_train', ...
+    'data_test', 'label_train', 'label_test', ...
+    'filename_train', 'filename_test');
